2b059245cfc82dc6a8d303dfb9b178c9b30a31ad
Abdul-Rehman <rehmankhan8360@gmail.com>
2025-05-13 16:05:28 +0500

    splash screen added

--- app/src/main/java/com/rehman/docscan/SplashActivity.kt
+++ app/src/main/java/com/rehman/docscan/SplashActivity.kt
@@ -1,16 +1,25 @@
 package com.rehman.docscan
 
+import android.annotation.SuppressLint
+import android.graphics.Color
 import android.os.Bundle
+import androidx.activity.SystemBarStyle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        this.installSplashScreen()
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.dark(getColor(R.color.color_primary)),
+            navigationBarStyle = SystemBarStyle.dark(getColor(R.color.color_primary))
+        )
+        setContentView(R.layout.activity_splash)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
