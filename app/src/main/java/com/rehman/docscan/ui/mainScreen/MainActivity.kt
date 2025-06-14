781e5c664708cf78565eb9557c5b74f3bb9ebe22
Abdul-Rehman <rehmankhan8360@gmail.com>
2025-06-14 19:12:26 +0500

    Introduce Crashlytics

--- app/src/main/java/com/rehman/docscan/ui/mainScreen/MainActivity.kt
+++ app/src/main/java/com/rehman/docscan/ui/mainScreen/MainActivity.kt
@@ -1,13 +1,20 @@
 package com.rehman.docscan.ui.mainScreen
 
+import android.graphics.Color
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.Gravity
+import android.view.ViewGroup
+import android.view.WindowManager
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.SystemBarStyle
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.drawable.toDrawable
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
@@ -38,6 +45,7 @@ class MainActivity : AppCompatActivity(), SnackBarListener {
             statusBarStyle = SystemBarStyle.dark(getColor(R.color.color_primary_variant)),
             navigationBarStyle = SystemBarStyle.dark(getColor(R.color.color_primary_variant))
         )
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
