8753370802ea7797f234cf97264854099079bbed
Abdul-Rehman <rehmankhan8360@gmail.com>
2025-06-24 16:18:30 +0500

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.rehman.docscan"
         minSdk = 24
         targetSdk = 35
-        versionCode = 14
-        versionName = "1.1.3"
+        versionCode = 15
+        versionName = "1.1.4"
 
         // Update what's new notes in distribution/whatsNew/whatsnew-en-US
 
